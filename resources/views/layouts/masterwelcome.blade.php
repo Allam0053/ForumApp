1cc502c7febed8d77ece0dcc6d20ab529e7c850a
Allam0053 <55141556+Allam0053@users.noreply.github.com>
2020-07-25 14:02:34 +0700

    button home di welcome

--- resources/views/layouts/masterwelcome.blade.php
+++ resources/views/layouts/masterwelcome.blade.php
@@ -76,6 +76,9 @@
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link btn btn-dark" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
+              </li>
               <li class="nav-item active" style="margin-right: 5px">
                 <!-- Button trigger modal -->
                 @yield('buttonlogin')
